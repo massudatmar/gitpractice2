fcd01f3f6705a800c8bfd93ded12950217076733
massudatmar <massud.atmar@gmail.com>
2020-04-28 15:36:45 -0400

    Ohio

---
+++ src/day_30/HappyBirthdayEveryone.java
@@ -0,0 +1,19 @@
+package day_30;
+
+public class HappyBirthdayEveryone {
+
+    public static void main(String[] args) {
+
+        boolean birthday = true;
+
+        if (birthday == true){
+
+            System.out.println("Turn up");
+        }
+        else {
+
+            System.out.println("Turn down");
+        }
+
+    }
+}
